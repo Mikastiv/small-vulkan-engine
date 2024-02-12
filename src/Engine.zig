49758d50e02ad8f12d592305bd66cb76fb370582
Mikastiv <mikastiv@outlook.com>
2024-02-12 00:19:44 -0500

    render pass and framebuffers

--- src/Engine.zig
+++ src/Engine.zig
@@ -24,6 +24,8 @@ swapchain_images: []vk.Image,
 swapchain_image_views: []vk.ImageView,
 command_pool: vk.CommandPool,
 main_command_buffer: vk.CommandBuffer,
+render_pass: vk.RenderPass,
+framebuffers: []vk.Framebuffer,
 
 pub fn init(allocator: Allocator) !@This() {
     if (c.glfwInit() == c.GLFW_FALSE) return error.GlfwInitFailed;
@@ -76,6 +78,17 @@ pub fn init(allocator: Allocator) !@This() {
     var command_buffer: vk.CommandBuffer = .null_handle;
     try vkd().allocateCommandBuffers(device.handle, &command_buffer_info, @ptrCast(&command_buffer));
 
+    const render_pass = try defaultRenderPass(device.handle, swapchain.image_format);
+    errdefer vkd().destroyRenderPass(device.handle, render_pass, null);
+
+    const framebuffers = try createFramebuffers(allocator, device.handle, render_pass, swapchain.extent, image_views);
+    errdefer {
+        for (framebuffers) |framebuffer| {
+            vkd().destroyFramebuffer(device.handle, framebuffer, null);
+            allocator.free(framebuffers);
+        }
+    }
+
     return .{
         .allocator = allocator,
         .window = window,
@@ -87,10 +100,17 @@ pub fn init(allocator: Allocator) !@This() {
         .swapchain_image_views = image_views,
         .command_pool = command_pool,
         .main_command_buffer = command_buffer,
+        .render_pass = render_pass,
+        .framebuffers = framebuffers,
     };
 }
 
 pub fn deinit(self: *@This()) void {
+    for (self.framebuffers) |framebuffer| {
+        vkd().destroyFramebuffer(self.device.handle, framebuffer, null);
+    }
+    self.allocator.free(self.framebuffers);
+    vkd().destroyRenderPass(self.device.handle, self.render_pass, null);
     vkd().destroyCommandPool(self.device.handle, self.command_pool, null);
     self.swapchain.destroyAndFreeImageViews(self.allocator, self.swapchain_image_views);
     self.allocator.free(self.swapchain_images);
@@ -123,6 +143,71 @@ pub fn run(self: *@This()) !void {
 
 fn draw(self: *@This()) !void {
     _ = self;
+}
+
+fn createFramebuffers(
+    allocator: Allocator,
+    device: vk.Device,
+    render_pass: vk.RenderPass,
+    extent: vk.Extent2D,
+    image_views: []const vk.ImageView,
+) ![]vk.Framebuffer {
+    var framebuffer_info = vk.FramebufferCreateInfo{
+        .render_pass = render_pass,
+        .attachment_count = 1,
+        .width = extent.width,
+        .height = extent.height,
+        .layers = 1,
+    };
+
+    var framebuffers = try std.ArrayList(vk.Framebuffer).initCapacity(allocator, image_views.len);
+    errdefer {
+        for (framebuffers.items) |framebuffer| {
+            vkd().destroyFramebuffer(device, framebuffer, null);
+        }
+        framebuffers.deinit();
+    }
+
+    for (0..image_views.len) |i| {
+        framebuffer_info.p_attachments = @ptrCast(&image_views[i]);
+        const framebuffer = try vkd().createFramebuffer(device, &framebuffer_info, null);
+        try framebuffers.append(framebuffer);
+    }
+
+    return framebuffers.toOwnedSlice();
+}
+
+fn defaultRenderPass(device: vk.Device, image_format: vk.Format) !vk.RenderPass {
+    const color_attachment = vk.AttachmentDescription{
+        .format = image_format,
+        .samples = .{ .@"1_bit" = true },
+        .load_op = .clear,
+        .store_op = .store,
+        .stencil_load_op = .dont_care,
+        .stencil_store_op = .dont_care,
+        .initial_layout = .undefined,
+        .final_layout = .present_src_khr,
+    };
+
+    const color_attachment_ref = vk.AttachmentReference{
+        .attachment = 0,
+        .layout = .color_attachment_optimal,
+    };
+
+    const subpass = vk.SubpassDescription{
+        .pipeline_bind_point = .graphics,
+        .color_attachment_count = 1,
+        .p_color_attachments = @ptrCast(&color_attachment_ref),
+    };
+
+    const render_pass_info = vk.RenderPassCreateInfo{
+        .attachment_count = 1,
+        .p_attachments = @ptrCast(&color_attachment),
+        .subpass_count = 1,
+        .p_subpasses = @ptrCast(&subpass),
+    };
+
+    return vkd().createRenderPass(device, &render_pass_info, null);
 }
 
 fn errorCallback(error_code: i32, description: [*c]const u8) callconv(.C) void {
